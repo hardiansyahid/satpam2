9a1e6c1d3d43df484737d7d880f0f72e3f9b3ab8
Hardiansyah <hardiansyahmail@gmail.com>
2024-10-03 21:09:48 +0700

    update : og meta

--- pages/index.vue
+++ pages/index.vue
@@ -5,27 +5,18 @@
 
 <script>
 import axios from 'axios';
-import { ref, onMounted } from 'vue';
-import { useHead, useSeoMeta } from '#app';
+import { ref, watch } from 'vue';
+import { useSeoMeta } from '#app';
 
 export default {
   async setup() {
     const data = ref({});
+    const title = ref('Home');
 
     const getData = async () => {
       try {
         const response = await axios.get('https://jsonplaceholder.typicode.com/photos');
         data.value = response.data[0];
-
-        // Set SEO meta tags after data is fetched
-        useSeoMeta({
-          ogTitle: data.value.title,
-          ogImage: data.value.url,  // Make sure this URL is accessible
-          ogDescription: data.value.title,
-          googlebot: 'index, follow',
-          description: 'Dukung pengawasan layanan publik bebas korupsi.',
-          keywords: 'jaga, kpk, jaga kpk, layanan publik',
-        });
       } catch (error) {
         console.error(error);
       }
@@ -34,10 +25,24 @@ export default {
     // Fetch data before rendering
     await getData();
 
+    // Watch for changes in data and update SEO meta tags when data is loaded
+    watch(data, (newData) => {
+      if (newData && newData.url) {
+        useSeoMeta({
+          ogTitle: newData.title,
+          ogImage: newData.url,
+          ogDescription: newData.title,
+          googlebot: 'index, follow',
+          description: 'Selamat datang di Satpam! Jelajahi dunia informasi dan layanan terlengkap untuk memenuhi segala kebutuhan Anda.',
+          keywords: 'satpam,satpam2,nextsatpam',
+        });
+      }
+    });
+
     return {
       data,
+      title,
     };
   },
 };
-
 </script>
