70350b25603a519edece3007995b58f4dc98574a
Hardiansyah <hardiansyahmail@gmail.com>
2024-10-03 20:42:01 +0700

    update : og meta

--- pages/about.vue
+++ pages/about.vue
@@ -1,14 +1,37 @@
 <template>
   <h1>{{title}}</h1>
+  <img :src="data.url"/>
 </template>
 
 <script>
+import axios from 'axios';
+
+useHead({
+  titleTemplate: 'About',
+})
+useSeoMeta({
+  ogTitle:this.data.title,
+  ogImage: this.data.url,
+  ogDescription:this.data.title,
+  googlebot: 'index, follow',
+  description : 'Dukung pengawasan layanan publik bebas korupsi. Tingkatkan transparansi pemerintah dengan partisipasi Anda. Kenali upaya pencegahan korupsi KPK di sini.',
+  keywords:"jaga, kpk, jaga kpk, jaringan pencegahan korupsi, kpk jaga, pelayanan publik, jaga pendidikan, jaga kesehatan, dana desa"
+})
 export default {
   name: 'About',
-  data(){
+  data() {
     return {
-      title : 'About',
+      title: 'About',
+      data: {}
     }
+  },
+  mounted() {
+    axios.get('https://jsonplaceholder.typicode.com/photos')
+        .then(response => {
+          this.data = response.data[0];
+        }).catch(reason => {
+      console.log(reason);
+    })
   }
 }
 </script>
