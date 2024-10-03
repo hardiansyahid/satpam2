ae4570fe99599f68aeba3531c4956d029496a61a
Hardiansyah <hardiansyahmail@gmail.com>
2024-10-03 21:04:23 +0700

    update : og meta

--- pages/index.vue
+++ pages/index.vue
@@ -9,51 +9,35 @@ import { ref, onMounted } from 'vue';
 import { useHead, useSeoMeta } from '#app';
 
 export default {
-  name: 'Home',
-  setup() {
-    const title = ref('Home');
+  async setup() {
     const data = ref({});
-    const list = ref([]);
 
-    // Dynamic meta tags
-    const setSeoMeta = () => {
-      useHead({
-        titleTemplate: 'Home Page',
-      });
-
-      useSeoMeta({
-        ogTitle: data.value.title,
-        ogImage: data.value.url,
-        ogDescription: data.value.title,
-        googlebot: 'index, follow',
-        description: 'Selamat datang di Satpam! Jelajahi dunia informasi dan layanan terlengkap untuk memenuhi segala kebutuhan Anda. Dari panduan praktis, tips dan trik, hingga berita terkini yang relevan, kami hadir untuk memberikan konten berkualitas yang menginspirasi. Dapatkan juga akses ke produk-produk inovatif, solusi bisnis, serta wawasan mendalam di berbagai bidang. Apapun yang Anda cari, Satpam adalah partner terpercaya untuk kesuksesan Anda. Mari bersama kami wujudkan impian dan tujuan Anda!',
-        keywords: 'satpam,satpam2,nextsatpam',
-      });
-    };
-
-    // Fetch data
     const getData = async () => {
       try {
         const response = await axios.get('https://jsonplaceholder.typicode.com/photos');
-        list.value = response.data;
         data.value = response.data[0];
 
-        // Set SEO meta tags dynamically after data is fetched
-        setSeoMeta();
+        // Set SEO meta tags after data is fetched
+        useSeoMeta({
+          ogTitle: data.value.title,
+          ogImage: data.value.url,  // Make sure this URL is accessible
+          ogDescription: data.value.title,
+          googlebot: 'index, follow',
+          description: 'Dukung pengawasan layanan publik bebas korupsi.',
+          keywords: 'jaga, kpk, jaga kpk, layanan publik',
+        });
       } catch (error) {
         console.error(error);
       }
     };
 
-    onMounted(() => {
-      getData();
-    });
+    // Fetch data before rendering
+    await getData();
 
     return {
-      title,
       data,
-      list,
     };
   },
 };
+
 </script>
